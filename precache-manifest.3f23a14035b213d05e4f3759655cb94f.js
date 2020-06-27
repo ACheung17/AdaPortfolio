eaab52d3c77e135413231a0e8269b200876c875c
Ada Cheung <ada.cheung96@gmail.com>
2020-06-26 20:45:36 -0400

    Updates

--- precache-manifest.3f23a14035b213d05e4f3759655cb94f.js
+++ precache-manifest.3f23a14035b213d05e4f3759655cb94f.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "57553bd08c2c7caea43a1afd368c9360",
+    "revision": "d1296770a9739abfb8ca3952df8766b7",
     "url": "/AdaPortfolio/index.html"
   },
   {
-    "revision": "c2d9d07e61171d0687c8",
+    "revision": "4240addef131e64ce2be",
     "url": "/AdaPortfolio/static/css/2.1d2230e4.chunk.css"
   },
   {
-    "revision": "c2d9d07e61171d0687c8",
-    "url": "/AdaPortfolio/static/js/2.3980a8d5.chunk.js"
+    "revision": "4240addef131e64ce2be",
+    "url": "/AdaPortfolio/static/js/2.e5717221.chunk.js"
   },
   {
     "revision": "5e9b6f5c16b720e8a86875f97734673e",
-    "url": "/AdaPortfolio/static/js/2.3980a8d5.chunk.js.LICENSE.txt"
+    "url": "/AdaPortfolio/static/js/2.e5717221.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "0d293667d298e26a3bbd",
-    "url": "/AdaPortfolio/static/js/main.aadc895c.chunk.js"
+    "revision": "d37b63fd847c194bfeda",
+    "url": "/AdaPortfolio/static/js/main.e90cbc12.chunk.js"
   },
   {
     "revision": "6e165826e1f525ca52d7",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/AdaPortfolio/precache-manifest.b1407aa2a8036a0646a5ec08518c2b1d.js"
+  "/AdaPortfolio/precache-manifest.3f23a14035b213d05e4f3759655cb94f.js"
 );
 
 self.addEventListener('message', (event) => {
